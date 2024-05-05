cce727e739dfd5a7d0506f518afa13cbd94389c7
Samotokhin Ilya <samotoxin@bkru>
2024-05-05 13:21:33 +0300

    Added color design

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const posts = []
 
+const TITLE_VALIDATION_LIMIT = 10
+const TEXT_VALIDATION_LIMIT = 20
+
 const postTitleInputNode = document.querySelector('.js-post-title-input')
 const postTextInputNode = document.querySelector('.js-post-text-input')
 const newPostBtnNode = document.querySelector('.js-new-post-btn')
 const postsNode = document.querySelector('.js-posts')
 const validationMassage = document.querySelector('.js-validationMassage')
+const countChar = document.querySelector('.js-count__char')
 
 newPostBtnNode.addEventListener('click', function () {
 	//получить данные из поля ввода
@@ -77,3 +81,41 @@ function getDate(date) {
 }
 
 //
+
+// validation
+
+postTitleInputNode.addEventListener('input', validation)
+postTextInputNode.addEventListener('input', validation)
+postTextInputNode.addEventListener('input', countingCharacters)
+
+function validation() {
+	const titleLen = postTitleInputNode.value.length
+	const textLen = postTextInputNode.value.length
+
+	if (titleLen > TITLE_VALIDATION_LIMIT) {
+		validationMassage.innerText = `Длинна заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`
+		validationMassage.classList.remove('validationMassage-hidden')
+		newPostBtnNode.disabled = true
+		return
+	}
+
+	if (textLen > TEXT_VALIDATION_LIMIT) {
+		validationMassage.innerText = `Длинна текста не должна превышать ${TEXT_VALIDATION_LIMIT} символов`
+		validationMassage.classList.remove('validationMassage-hidden')
+		newPostBtnNode.disabled = true
+		return
+	}
+
+	if (titleLen < 1 || textLen < 1) {
+		newPostBtnNode.disabled = true
+		return
+	}
+
+	validationMassage.classList.add('validationMassage-hidden')
+	newPostBtnNode.disabled = false
+}
+
+//counter
+function countingCharacters() {
+	countChar.innerText = `${postTextInputNode.value.length}/${TEXT_VALIDATION_LIMIT}`
+}
